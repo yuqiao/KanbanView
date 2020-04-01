212cd8ca09d7772e3f8fdf3a580a9eb9301a4efe
Alexander Willner <alex@willner.ws>
2020-04-02 00:54:45 +0200

    cleanup and added auto-refresh for server mode

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,42 +1,22 @@
+"use strict";
+/*jslint browser: true*/
+/*global $*/
+
 function xml_http_post(url, data, callback) {
-    var req = false;
-    try {
-        // Firefox, Opera 8.0+, Safari
-        req = new XMLHttpRequest();
-    }
-    catch (e) {
-        // Internet Explorer
-        try {
-            req = new ActiveXObject("Msxml2.XMLHTTP");
-        }
-        catch (e) {
-            try {
-                req = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            catch (e) {
-                alert("Your browser does not support AJAX!");
-                return false;
-            }
-        }
-    }
+    var req = new XMLHttpRequest();
     req.open("POST", url, true);
-    req.onreadystatechange = function() {
-        if (req.readyState == 4) {
-            callback(req);
-        }
-    }
+    req.onreadystatechange = function () { if (req.readyState === 4) { callback(req); } };
     req.send(data);
 }
 
-function refresh() {
-    var elem = document.getElementById('content')
-    elem.innerHTML = ""
-    xml_http_post("kanban.html", 99, add_content)
-}
-
 function add_content(req) {
-    var elem = document.getElementById('content')
-    elem.innerHTML =  req.responseText
+    var elem = document.getElementById('content');
+    elem.innerHTML = req.responseText;
 }
 
-xml_http_post("kanban.html", 99, add_content)
+function refresh() {
+    xml_http_post(null, null, add_content);
+}
+
+window.onfocus = refresh;
+window.onload = refresh;
