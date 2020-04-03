4693c5f85bf09689a2c9461046459ef07ab4054d
Alexander Willner <alex@willner.ws>
2020-04-03 20:23:56 +0200

    refactored parsing, added JSON support

--- src/things3_cli.py
+++ src/things3_cli.py
@@ -15,17 +15,34 @@ __email__ = "alex@willner.ws"
 __status__ = "Development"
 
 import argparse
+import json
 import things3
 
 
 class Things3CLI():
     """Simple read-only Thing 3 CLI."""
 
-    @classmethod
-    def print_tasks(cls, tasks):
+    I_UUID = 0
+    I_TITLE = 1
+    I_CONTEXT = 2
+    I_CONTEXT_UUID = 3
+    print_json = False
+
+    def __init__(self, print_json):
+        self.print_json = print_json
+
+    def print_tasks(self, tasks):
         """Print a task."""
         for task in tasks:
-            print(' - ' + task[1])
+            title = task[self.I_TITLE]
+            context = str(task[self.I_CONTEXT])
+            if self.print_json:
+                print(json.dumps([{
+                    'title': title,
+                    'context': context
+                    }]))
+            else:
+                print(' - ' + title + ' (' + context + ')')
 
     def print_today(self):
         """Show Today."""
@@ -43,115 +60,99 @@ class Things3CLI():
 
 def main(args):
     """ Main entry point of the app """
-    COMMANDS[args.command][1]()
+    things = Things3CLI(args.json)
+    if args.command == "inbox":
+        things.print_inbox()
+    elif args.command == "today":
+        things.print_today()
+    else:
+        Things3CLI.print_unimplemented()
 
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter)
-    COMMANDS = {
-        'inbox': ['Shows all inbox tasks',
-                  Things3CLI().print_inbox],
-        'today': ['Shows all todays tasks ordered by index',
-                  Things3CLI().print_today],
-        'upcoming': ['Shows all upcoming tasks ordered by date',
-                     Things3CLI().print_unimplemented],
-        'next': ['Shows all next tasks',
-                 Things3CLI().print_unimplemented],
-        'someday': ['Shows all someday tasks',
-                    Things3CLI().print_unimplemented],
-        'completed': ['Shows all completed tasks',
-                      Things3CLI().print_unimplemented],
-        'cancelled': ['Shows all cancelled tasks ordered by cancel date',
-                      Things3CLI().print_unimplemented],
-        'trashed': ['Shows all trashed tasks',
-                    Things3CLI().print_unimplemented],
-        'feedback': ['Opens the feedback web page to propose changes',
-                     Things3CLI().print_unimplemented],
-        'all': ['Shows all tasks',
-                Things3CLI().print_unimplemented],
-        'csv': ['Exports all tasks as semicolon seperated values',
-                Things3CLI().print_unimplemented],
-        'due': ['Shows all tasks ordered by due date',
-                Things3CLI().print_unimplemented],
-        'headings': ['Shows all headings',
-                     Things3CLI().print_unimplemented],
-        'hours': ['Shows how many hours of work have been planned for today',
-                  Things3CLI().print_unimplemented],
-        'ical': ['Shows all tasks ordered by due date as iCal',
-                 Things3CLI().print_unimplemented],
-        'logbook': ['Shows all tasks completed today',
-                    Things3CLI().print_unimplemented],
-        'mostClosed': ['Shows all days on which most tasks were closed',
-                       Things3CLI().print_unimplemented],
-        'mostCancelled': ['Shows all days on which most tasks were cancelled',
-                          Things3CLI().print_unimplemented],
-        'mostTrashed': ['Shows all days on which most tasks were trashed',
-                        Things3CLI().print_unimplemented],
-        'mostCreated': ['Shows all days on which most tasks were created',
-                        Things3CLI().print_unimplemented],
-        'mostTasks': ['Shows all projects that have most tasks',
-                      Things3CLI().print_unimplemented],
-        'mostCharacters': ['Shows all tasks that have most characters',
-                           Things3CLI().print_unimplemented],
-        'nextish': ['Shows all nextish tasks',
-                    Things3CLI().print_unimplemented],
-        'old': ['Shows all old tasks',
-                Things3CLI().print_unimplemented],
-        'projects': ['Shows all projects',
-                     Things3CLI().print_unimplemented],
-        'repeating': ['Shows all repeating tasks',
-                      Things3CLI().print_unimplemented],
-        'schedule': ['Schedule an event by creating a number of related tasks',
-                     Things3CLI().print_unimplemented],
-        'search': ['Searches for a specific task',
-                   Things3CLI().print_unimplemented],
-        'stat': ['Provides a number of statistics about all tasks',
-                 Things3CLI().print_unimplemented],
-        'statcsv': ['Exports some statistics as semicolon separated values',
-                    Things3CLI().print_unimplemented],
-        'subtasks': ['Shows all subtasks',
-                     Things3CLI().print_unimplemented],
-        'tag': ['Shows all tasks with the waiting for tag',
-                Things3CLI().print_unimplemented],
-        'tags': ['Shows all tags ordered by their usage',
-                 Things3CLI().print_unimplemented],
-        'waiting': ['Shows all tasks with the waiting for tag',
-                    Things3CLI().print_unimplemented],
-    }
-    CHOICES = []
-    DOCU = ""
-    for command in COMMANDS:
-        CHOICES.append(command)
-        DOCU = DOCU + command + "\t\t" + (COMMANDS[command][0]) + "\n"
+        description='Simple read-only Thing 3 CLI.')
 
-    # Required positional argument
-    PARSER.add_argument('command', choices=CHOICES,
-                        metavar='command', help=DOCU)
+    SUBPARSERS = PARSER.add_subparsers(help='One of the following commands:',
+                                       metavar="command",
+                                       required=True,
+                                       dest="command")
+    SUBPARSERS.add_parser('inbox',
+                          help='Shows all inbox tasks')
+    SUBPARSERS.add_parser('today',
+                          help='Shows all todays tasks')
+    SUBPARSERS.add_parser('upcoming',
+                          help='Shows all upcoming tasks')
+    SUBPARSERS.add_parser('next',
+                          help='Shows all next tasks')
+    SUBPARSERS.add_parser('someday',
+                          help='Shows all someday tasks')
+    SUBPARSERS.add_parser('completed',
+                          help='Shows all completed tasks')
+    SUBPARSERS.add_parser('cancelled',
+                          help='Shows all cancelled tasks')
+    SUBPARSERS.add_parser('trashed',
+                          help='Shows all trashed tasks')
+    SUBPARSERS.add_parser('feedback',
+                          help='Give feedback')
+    SUBPARSERS.add_parser('all',
+                          help='Shows all tasks')
+    SUBPARSERS.add_parser('csv',
+                          help='Exports all tasks as CSV')
+    SUBPARSERS.add_parser('due',
+                          help='Shows all tasks with due dates')
+    SUBPARSERS.add_parser('headings',
+                          help='Shows all headings')
+    SUBPARSERS.add_parser('hours',
+                          help='Shows how many hours have been planned today')
+    SUBPARSERS.add_parser('ical',
+                          help='Shows all tasks ordered by due date as iCal')
+    SUBPARSERS.add_parser('logbook',
+                          help='Shows all tasks completed today')
+    SUBPARSERS.add_parser('mostClosed',
+                          help='Shows days on which most tasks were closed')
+    SUBPARSERS.add_parser('mostCancelled',
+                          help='Shows days on which most tasks were cancelled')
+    SUBPARSERS.add_parser('mostTrashed',
+                          help='Shows days on which most tasks were trashed')
+    SUBPARSERS.add_parser('mostCreated',
+                          help='Shows days on which most tasks were created')
+    SUBPARSERS.add_parser('mostTasks',
+                          help='Shows projects that have most tasks')
+    SUBPARSERS.add_parser('mostCharacters',
+                          help='Shows tasks that have most characters')
+    SUBPARSERS.add_parser('nextish',
+                          help='Shows all nextish tasks')
+    SUBPARSERS.add_parser('old',
+                          help='Shows all old tasks')
+    SUBPARSERS.add_parser('projects',
+                          help='Shows all projects')
+    SUBPARSERS.add_parser('repeating',
+                          help='Shows all repeating tasks')
+    SUBPARSERS.add_parser('schedule',
+                          help='Schedules an event using a template')
+    SUBPARSERS.add_parser('search',
+                          help='Searches for a specific task')
+    SUBPARSERS.add_parser('stat',
+                          help='Provides a number of statistics')
+    SUBPARSERS.add_parser('statcsv',
+                          help='Exports some statistics as CSV')
+    SUBPARSERS.add_parser('subtasks',
+                          help='Shows all subtasks')
+    SUBPARSERS.add_parser('tag',
+                          help='Shows all tasks with the waiting for tag')
+    SUBPARSERS.add_parser('tags',
+                          help='Shows all tags ordered by their usage')
+    SUBPARSERS.add_parser('waiting',
+                          help='Shows all tasks with the waiting for tag')
 
-    # Optional argument which requires a parameter (eg. -d test)
-    PARSER.add_argument("-l", action="store", dest="limit_by",
-                        help="Limit output by <number> of results")
-    PARSER.add_argument("-w", action="store", dest="waiting_tag",
-                        help="Set waiting/filter tag to <tag>")
-    PARSER.add_argument("-o", action="store", dest="order_by",
-                        help="Sort output by <column> (e.g. 'creationDate')")
-    PARSER.add_argument("-s", action="store", dest="search",
-                        help="String <string> to search for")
-    PARSER.add_argument("-r", action="store", dest="range",
-                        help="Limit CSV statistic export by <string>")
-    PARSER.add_argument("-e", action="store", dest="eventfile",
-                        help="Event: <filename> that contains a list of tasks")
-    PARSER.add_argument("-t", action="store", dest="start",
-                        help="Event: starts at <date>")
-    PARSER.add_argument("-d", action="store", dest="duration",
-                        help="Event: ends after <days>")
-
-    # Specify output of "--version"
+    PARSER.add_argument("-j", "--json",
+                        action="store_true", default=False,
+                        help="output as JSON", dest="json")
     PARSER.add_argument(
         "--version",
         action="version",
         version="%(prog)s (version {version})".format(version=__version__))
 
-    MYARGS = PARSER.parse_args()
-    main(MYARGS)
+    ARGUMENTS = PARSER.parse_args()
+    main(ARGUMENTS)
