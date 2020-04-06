b6cc82aaee95222096e4c2284466946c7e779140
Alexander Willner <alex@willner.ws>
2020-04-06 13:15:14 +0200

    final fixes for release 2.0

--- setup.py
+++ setup.py
@@ -4,24 +4,31 @@ py2app configuration file.
 
 from setuptools import setup
 
+VERSION = "2.0.0"
+APP_NAME="KanbanView"
 APP = ['src/things3_app.py']
-DATA_FILES = [('resources', ['resources/*']),
+DATA_FILES = [('resources', ["resources/logo.png"]),
+              ('resources', ["resources/kanban.js"]),
+              ('resources', ["resources/kanban.css"]),
+              ('resources', ["resources/kanban.html"]),
+              ('resources', ["resources/favicon.ico"]),
               'src/things3.py', 'src/things3_api.py']
 OPTIONS = {
         'argv_emulation': True,
-        'plist': {'CFBundleName': "KanbanView",
-                  'CFBundleDisplayName': "KanbanView",
-                  'CFBundleGetInfoString': "KanbanView",
+        'plist': {'CFBundleName': APP_NAME,
+                  'CFBundleDisplayName': APP_NAME,
+                  'CFBundleGetInfoString': APP_NAME,
                   'CFBundleIdentifier': "ws.willner.kanbanview",
-                  'CFBundleVersion': "2.0.0",
-                  'CFBundleShortVersionString': "2.0.0",
+                  'CFBundleVersion': VERSION,
+                  'CFBundleShortVersionString': VERSION,
                   'NSHumanReadableCopyright':'Copyright 2020 Alexander Willner'},
         'optimize':'2'
 }
 
 setup(
     app=APP,
-    name='KanbanView',
+    name=APP_NAME,
+    version=VERSION,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
