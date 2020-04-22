dbb3684bce2f5dea69a867962993184c66330409
Alexander Willner <alex@willner.ws>
2020-04-22 12:11:54 +0200

    version 2.5.0.dev1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ AUTHOR = "Alexander Willner"
 AUTHOR_MAIL = "alex@willner.ws"
 DESCRIPTON = "A simple read-only CLI, API and Web Service for Things 3"
 URL = "https://github.com/alexanderwillner/kanbanview"
-VERSION = "2.5.0.dev0"
+VERSION = "2.5.0.dev1"
 DATA_FILES = [('resources', ["resources/logo.png"]),
               ('resources', ["resources/logo-dark.png"]),
               ('resources', ["resources/kanban.js"]),
